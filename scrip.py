4501c82234afe314435ea7b00cfe6bc3b2aafa7c
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-05-06 11:30:48 +0300

    Add files via upload

--- scrip.py
+++ scrip.py
@@ -1,20 +1,23 @@
 class Num:
-    def __init__(self, x, y):
+    def __init__(self, x,y):
         self.x = x
         self.y = y
     
-    def __add__(self, value):
-        return Num(self.x + value.x, self.y - value.y )
+    def __add__(self,  value):
+        return Num(self.x + value.x, self.y * value.y)
 
     def __repr__(self):
-        return f'X {self.x}ga teng Y {self.y}ga teng'
+        return f'X teng{self.x} Y {self.y}ga teng'
 
 
-num = Num(10,25)
-num1  = Num(25,14)
+num = Num(25, 55)
 
-result = num + num1
+
+result = num + num + num
+
 print(result)
 
-#result X 35ga teng Y 11ga teng
+
+
+
 
